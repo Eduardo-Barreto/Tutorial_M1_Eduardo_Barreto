72f6a3432d9697cd46809835d3ce52044425e0af
Eduardo Santos Barreto <eduardo.barreto@sou.inteli.edu.br>
2023-02-01 21:25:44 -0300

    fix: more descriptive variable names

--- SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
+++ SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
@@ -1,9 +1,9 @@
 extends Node # instancia a classe Node2D
 
-var status = 1 # indica se o usuário está jogando ou parado (como um bool, mas representado com inteiros)
-var vscore = 0 # score do usuário, incrementada a cada coluna superada
-var x = 1.5 # "velocidade" de movimentação do dragão (decremento da posição do cenário)
-var y = 1.5 # "força gravidade", incremento na posição em y para puxar o dragão para baixo
+var playing = true # indica se o usuário está jogando ou parado (como um bool, mas representado com inteiros)
+var player_score = 0 # score do usuário, incrementada a cada coluna superada
+var velocity = 1.5 # "velocidade" de movimentação do dragão (decremento da posição do cenário)
+var gravity = 1.5 # "força gravidade", incremento na posição em y para puxar o dragão para baixo
 
 # executa essa função ao carregar o jogo
 func _ready():
@@ -14,25 +14,25 @@ func _ready():
 # executa essa função a cada frame (60 FPS)
 func _process(delta):
 	
-	if status == 1: # jogando
+	if playing: # jogando
 		# movimenta o cenário do fundo
-		$background.position.x -= 1*x
+		$background.position.x -= velocity
 		if ($background.position.x) < -200:
 			$background.position.x = 600
 			
 		# movimenta as colunas para colisão
-		$columns.position.x -= 2*x
+		$columns.position.x -= 2*velocity
 		if ($columns.position.x) < -550:
 			$columns.position.x = rand_range(0, 350) - 50
 			$columns.position.y = rand_range(0, 400) - 200
 		
 		# puxa o dragão para baixo
-		$dragon.position.y += y
+		$dragon.position.y += gravity
 
 		# se bateu no fundo, não desce mais e termina o jogo
 		if $dragon.position.y > 480:
 			$dragon.position.y = 480
-			status = 0 # muda o status para "parado"
+			playing = false # muda o status para "parado"
 
 		# se bateu no teto, não sobe mais
 		if $dragon.position.y < -20:
@@ -44,31 +44,31 @@ func _process(delta):
 
 		# se apertou seta para cima, diminui o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_up"):
-			$dragon.position.y -= 4
+			$dragon.position.y -= 2.5 * gravity
 			
-	elif status == 0: # parado
+	else: # parado
 		$dragon/dragonImages.playing = false # faz dragão parar de bater as asas
 		$gameover.show() # exibe imagem gameover
 
 		# se apertou enter ou space, recomeça o jogo
 		if Input.is_action_pressed("ui_accept"):
-			$score.set_text("0") # zera o score
-			vscore = 0 # zera o score
-			status = 1 # muda o status para "jogando"
+			player_score = 0 # zera o score
+			$score.set_text("0") # zera o score no display
 			$dragon/dragonImages.playing = true # faz dragão voltar a bater as asas
 			$dragon.position.y = 0 # volta o dragão para a posição original
 			$columns.position.x = 400 # muda a posição das colunas
 			$gameover.hide() # oculta o gameover
+			playing = true # muda o status para "jogando"
 
 			
 
 # executa essa função quando o dragão bate na coluna
 func _on_columns_body_shape_entered(body_id, body, body_shape, local_shape):
 	if (local_shape < 2): # esse node tem 3 shapes de colisão: 0 e 1 são as colunas
-		status = 0 # muda o status para "parado"
+		playing = false # muda o status para "parado"
 
 # executa essa função quando o dragão atravessa entre as colunas
 func _on_columns_body_shape_exited(body_id, body, body_shape, local_shape):
 	if (local_shape == 2): # esse node tem 3 shapes de colisão: 0 e 1 são as colunas
-		vscore += 1 # aumenta o score
-		$score.set_text(str(vscore)) # atualiza o painel
+		player_score += 1 # aumenta o score
+		$score.set_text(str(player_score)) # atualiza o painel
