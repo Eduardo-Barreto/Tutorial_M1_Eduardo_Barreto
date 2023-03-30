289b8ac7dd79def0d8da992af315304c274bdab0
Eduardo Santos Barreto <eduardo.barreto@sou.inteli.edu.br>
2023-03-30 09:37:29 -0300

    fix: animate instruction

--- SEMANA_09/02_ATIVIDADES/Sorting/scripts/card.gd
+++ SEMANA_09/02_ATIVIDADES/Sorting/scripts/card.gd
@@ -7,36 +7,6 @@ var unsorted_color = Color(0.984375, 0.800781, 0.378906, 1)
 var rng = RandomNumberGenerator.new()
 var tween = Tween.new()
 
-func change_number(new_number):
-	number = new_number
-
-	$top_number.text = str(number)
-	$center_number.text = str(number)
-	$bottom_number.text = str(number)
-
-	tween.interpolate_property(
-		self,
-		'position:y',
-		initial_y,
-		position.y - 10,
-		.25,
-		Tween.TRANS_SINE,
-		Tween.EASE_IN_OUT
-	)
-
-	tween.interpolate_property(
-		self,
-		'position:y',
-		position.y - 10,
-		initial_y,
-		.25,
-		Tween.TRANS_SINE,
-		Tween.EASE_IN_OUT,
-		.25
-	)
-
-	tween.start()
-
 
 func change_color(color):
 	$back.color = color
@@ -48,44 +18,41 @@ func reset():
 	change_color(unsorted_color)
 
 
+func _ready():
+	add_child(tween)
+
+
 func change_position(index):
 	var new_position = index*290 + 235
 
 	tween.interpolate_property(
-		self,
-		'position:y',
-		initial_y,
-		exchange_position,
-		1,
-		Tween.TRANS_SINE,
-		Tween.EASE_IN_OUT
+		self, 'position:y', initial_y, exchange_position, 1, Tween.TRANS_SINE, Tween.EASE_IN_OUT
 	)
 
 	tween.interpolate_property(
-		self,
-		'position:x',
-		position.x,
-		new_position,
-		1,
-		Tween.TRANS_SINE,
-		Tween.EASE_IN_OUT,
-		1
+		self, 'position:x', position.x, new_position, 1, Tween.TRANS_SINE, Tween.EASE_IN_OUT, 1
 	)
 
 	tween.interpolate_property(
-		self,
-		'position:y',
-		exchange_position,
-		initial_y,
-		1,
-		Tween.TRANS_SINE,
-		Tween.EASE_IN_OUT,
-		2
+		self, 'position:y', exchange_position, initial_y, 1, Tween.TRANS_SINE, Tween.EASE_IN_OUT, 2
 	)
 
 	tween.start()
 
 
+func change_number(new_number):
+	number = new_number
 
-func _ready():
-	add_child(tween)
+	$top_number.text = str(number)
+	$center_number.text = str(number)
+	$bottom_number.text = str(number)
+
+	tween.interpolate_property(
+		self, 'position:y', initial_y, position.y - 10, .25, Tween.TRANS_SINE, Tween.EASE_IN_OUT
+	)
+
+	tween.interpolate_property(
+		self, 'position:y', position.y - 10, initial_y, .25, Tween.TRANS_SINE, Tween.EASE_IN_OUT, .25
+	)
+
+	tween.start()
--- SEMANA_09/02_ATIVIDADES/Sorting/scripts/main.gd
+++ SEMANA_09/02_ATIVIDADES/Sorting/scripts/main.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-var unsorted_color = Color(0.984375, 0.800781, 0.378906, 1)
-var active_color = Color(0.007843, 0.611765, 0.835294)
-var wrong_color = Color(0.901961, 0.388235, 0.407843, 1)
-var right_color = Color(0.647059, 0.788235, 0.490196, 1)
 var rng = RandomNumberGenerator.new()
 var tween = Tween.new()
 var sorting_methods = ["bubble_sort", "insertion_sort"]
@@ -14,9 +10,12 @@ var sorting = false
 
 onready var cards = get_tree().get_nodes_in_group('card')
 
-func _ready():
-	add_child(tween)
-	generate_new_list()
+var colors = {
+	'unsorted': Color(0.984375, 0.800781, 0.378906, 1),
+	'active': Color(0.007843, 0.611765, 0.835294),
+	'wrong': Color(0.901961, 0.388235, 0.407843, 1),
+	'right': Color(0.647059, 0.788235, 0.490196, 1)
+}
 
 
 func generate_new_list():
@@ -34,53 +33,12 @@ func swap(index1, index2):
 	cards[index2] = temp
 
 
-func hide_buttons():
-	tween.interpolate_property(
-		$left_button,
-		'position:y',
-		890,
-		1200,
-		Tween.TRANS_SINE,
-		Tween.EASE_IN_OUT
-	)
-	tween.interpolate_property(
-		$right_button,
-		'position:y',
-		890,
-		1200,
-		Tween.TRANS_SINE,
-		Tween.EASE_IN_OUT
-	)
-	tween.start()
-
-
-func show_buttons():
-	tween.interpolate_property(
-		$left_button,
-		'position:y',
-		1200,
-		890,
-		Tween.TRANS_SINE,
-		Tween.EASE_IN_OUT
-	)
-	tween.interpolate_property(
-		$right_button,
-		'position:y',
-		1200,
-		890,
-		Tween.TRANS_SINE,
-		Tween.EASE_IN_OUT
-	)
-	tween.start()
-
-
-
 func start_sort():
 	if sorting:
 		return
 
-	$'Panel/left_button'.color = active_color
-	$'Panel/right_button'.color = active_color
+	$'Panel/left_button'.color = colors.active
+	$'Panel/right_button'.color = colors.active
 
 	sorting = true
 	hide_buttons()
@@ -92,7 +50,7 @@ func start_sort():
 func bubble_sort():
 	for _i in range(len(cards)-1):
 		for card in range(len(cards)-1):
-			cards[card].change_color(active_color)
+			cards[card].change_color(colors.active)
 			var current_card = cards[card].number
 			var next_card = cards[card+1].number
 
@@ -101,7 +59,7 @@ func bubble_sort():
 				card += 1
 				yield(get_tree().create_timer(3), "timeout")
 
-			cards[card].change_color(unsorted_color)
+			cards[card].change_color(colors.unsorted)
 
 	emit_signal('sort_finished')
 
@@ -109,7 +67,7 @@ func bubble_sort():
 func insertion_sort():
 	for i in range(1, len(cards)):
 		var current_card = i
-		cards[current_card].change_color(active_color)
+		cards[current_card].change_color(colors.active)
 		var j = i
 
 		while j > 0 and cards[j-1].number > cards[j].number:
@@ -118,7 +76,7 @@ func insertion_sort():
 			j -= 1
 			yield(get_tree().create_timer(3.2), "timeout")
 
-		cards[current_card].change_color(unsorted_color)
+		cards[current_card].change_color(colors.unsorted)
 
 	emit_signal('sort_finished')
 
@@ -132,12 +90,12 @@ func choice_insertion():
 		return
 
 	if sort_method != 'insertion_sort':
-		$'Panel/left_button'.color = wrong_color
-		$'Panel/right_button'.color = right_color
+		$'Panel/left_button'.color = colors.wrong
+		$'Panel/right_button'.color = colors.right
 		$'Panel/right_button'.color.a = 0.5
 	else:
-		$'Panel/left_button'.color = right_color
-		$'Panel/right_button'.color = wrong_color
+		$'Panel/left_button'.color = colors.right
+		$'Panel/right_button'.color = colors.wrong
 		$'Panel/right_button'.color.a = 0.5
 
 	sorting = false
@@ -146,18 +104,19 @@ func choice_insertion():
 	$Panel.visible = false
 	generate_new_list()
 	show_buttons()
+
 
 func choose_bubble():
 	if not sorting:
 		return
 
 	if sort_method != 'bubble_sort':
-		$'Panel/right_button'.color = wrong_color
-		$'Panel/left_button'.color = right_color
+		$'Panel/right_button'.color = colors.wrong
+		$'Panel/left_button'.color = colors.right
 		$'Panel/left_button'.color.a = 0.5
 	else:
-		$'Panel/right_button'.color = right_color
-		$'Panel/left_button'.color = wrong_color
+		$'Panel/right_button'.color = colors.right
+		$'Panel/left_button'.color = colors.wrong
 		$'Panel/left_button'.color.a = 0.5
 
 	sorting = false
@@ -166,3 +125,36 @@ func choose_bubble():
 	$Panel.visible = false
 	generate_new_list()
 	show_buttons()
+
+
+func _ready():
+	add_child(tween)
+	generate_new_list()
+
+
+func hide_buttons():
+	tween.interpolate_property(
+		$left_button, 'position:y', 890, 1200, Tween.TRANS_SINE, Tween.EASE_IN_OUT
+	)
+	tween.interpolate_property(
+		$right_button, 'position:y', 890, 1200, Tween.TRANS_SINE, Tween.EASE_IN_OUT
+	)
+	tween.interpolate_property(
+		$instruction, 'rect_position:y', 110, -240, Tween.TRANS_SINE, Tween.EASE_IN_OUT
+	)
+
+	tween.start()
+
+
+func show_buttons():
+	tween.interpolate_property(
+		$left_button, 'position:y', 1200, 890, Tween.TRANS_SINE, Tween.EASE_IN_OUT
+	)
+	tween.interpolate_property(
+		$right_button, 'position:y', 1200, 890, Tween.TRANS_SINE, Tween.EASE_IN_OUT
+	)
+	tween.interpolate_property(
+		$instruction, 'rect_position:y', -240, 100, Tween.TRANS_SINE, Tween.EASE_IN_OUT
+	)
+
+	tween.start()
